cf864d1e404f9b89613afc976a3f0001f0618bd8
mmhs2 <m.m.hs260056@ezweb.ne.jp>
2021-10-22 12:38:48 +0000

    bookers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+       redirect_to @book, notice: 'Book was successfully created.'
+    else
+      render :index
+    end
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_url, notice: 'Book was successfully destroyed.'
+  end
+
+  private
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+end
